dfd079576a4b2897f9a511f3a6081adc97e88655
Unknown <gaz492@gmail.com>
2017-08-11 12:43:32 +0100

    FAQ

---
+++ config/default.js
@@ -0,0 +1,34 @@
+/**
+ * Created by Gareth on 10/08/2017.
+ */
+
+
+const config = {
+    server: {
+        port : 3000,
+        debug: true
+    },
+    web: {
+        title: "FTB FAQ",
+        siteName: "FTB Knowledge Base"
+    },
+    faqTopics: {
+        data: [
+            { uri: "tLog", category: "twitch", name: "Twitch log upload", title: "Uploading logs.hbs when using the Twitch app", url: "https://support.feed-the-beast.com/faq/tLog", enabled: true },
+            { uri: "dlTwitch", category: "twitch", name: "Downloading FTB packs on Twitch", title: "Downloading FTB packs with the Twitch app", url: "https://support.feed-the-beast.com/faq/dlTwitchFTB", enabled: true },
+            { uri: "logs.hbs", category: "legacy", name: "Upload logs.hbs with FTB launcher", title: "Uploading logs.hbs with the FTB launcher", url: "https://support.feed-the-beast.com/faq/legacyLogs", enabled: true },
+            { uri: "twitchBug", category: "twitch", name: "Reporting Twitch app issues", title: "Reporting issues with the Twitch app", url: "https://support.feed-the-beast.com/faq/twitchBug", enabled: true },
+            { uri: "dlServer", category: "server", name: "Downloading server files", title: "Downloading Server Files", url: "https://support.feed-the-beast.com/faq/downloadServer", enabled: true },
+            { uri: "twitchImport", category: "twitch", name: "Importing packs", title: "Importing Packs", url: "https://support.feed-the-beast.com/faq/twitchImport", enabled: true },
+            { uri: "forceUpdate", category: "legacy", name: "Force update", title: "Force Update", url: "https://support.feed-the-beast.com/faq/forceUpdate", enabled: true },
+            { uri: "mcSettings", category: "twitch", name: "Twitch Minecraft settings", title: "Changing Minecraft settings with the Twitch app", url: "https://support.feed-the-beast.com/faq/mcSettingsTwitch", enabled: true },
+            { uri: "twitchPackRepair", category: "twitch", name: "Repair Twitch pack", title: "Repairing modpacks on the Twitch app", url: "https://support.feed-the-beast.com/faq/twitchPackRepair", enabled: true },
+            { uri: "serverSetup", category: "server", name: "Setting up a server", title: "Setting up a server", url: "https://support.feed-the-beast.com/faq/serverSetup", enabled: true },
+            { uri: "serverUpdate.hbs", category: "server", name: "Updating a server", title: "Updating a server", url: "https://support.feed-the-beast.com/faq/serverUpdate.hbs", enabled: true },
+            { uri: "j8u51", category: "faq", name: "Windows 10 Intel driver issues", title: "Windows 10 Intel driver issues", url: "https://support.feed-the-beast.com/faq/intelJ8U51", enabled: true }
+        ]
+    }
+
+};
+
+module.exports = config;
---
+++ routes/index.js
@@ -0,0 +1,78 @@
+var express = require('express');
+var lodash = require('lodash');
+var router = express.Router();
+const config = require('../config/default');
+const topics = config.faqTopics.data;
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    var topicDetails = lodash.filter(topics, {'enabled': true} );
+    var twitchTopics = [];
+    var serverTopics = [];
+    var legacyTopics = [];
+    var faqTopics = [];
+
+    for(x in topicDetails){
+        // console.log(x);
+        if(topicDetails[x].category === "twitch"){
+            twitchTopics.push(topicDetails[x])
+        }else if(topicDetails[x].category === "legacy"){
+            legacyTopics.push(topicDetails[x])
+        }else if(topicDetails[x].category === "server"){
+            serverTopics.push(topicDetails[x])
+        }else if(topicDetails[x].category === "faq"){
+            faqTopics.push(topicDetails[x])
+        }
+    }
+    res.render('index', {
+        siteInfo: {
+            title: config.web.title,
+            siteName: config.web.siteName
+        },
+        topics:{
+            twitch: twitchTopics,
+            server: serverTopics,
+            legacy: legacyTopics,
+            faq: faqTopics
+        },
+        pageName: "Home",
+        home: true
+    });
+});
+
+router.get('/sitejson.json', function(req, res, next) {
+    var siteJSON = JSON.stringify( config.faqTopics );
+    res.header("Content-Type", "application/json");
+    res.render('json', {
+        json: siteJSON
+    });
+});
+
+router.get('/:topic', function(req, res, next) {
+    var topicParam = req.params.topic;
+    var topicDetails = lodash.find(topics, {'uri': topicParam} );
+    if((typeof topicDetails !== 'undefined') && topicDetails.enabled){
+        res.render('faq', {
+            siteInfo: {
+                title: config.web.title,
+                siteName: config.web.siteName
+
+            },
+            topicInfo:{
+                title: topicDetails.name,
+                description: topicDetails.title,
+                category: topicDetails.category
+            },
+            home: false,
+            getTopic: function() {
+                return "topics/" + topicParam;
+            }
+        });
+    }else{
+        var err = new Error('Not Found');
+        err.status = 404;
+        next(err);
+    }
+});
+
+module.exports = router;
